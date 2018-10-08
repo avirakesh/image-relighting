951410dde4b6c8336a7576abd2544fae8f6f5cef
jack <jchen488@wisc.edu>
2018-10-07 19:24:45 -0500

    displaying a square

---
+++ fShader.js
@@ -0,0 +1,5 @@
+const fsSource = `
+void main() {
+  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+`;
---
+++ vShader.js
@@ -0,0 +1,12 @@
+  // Vertex shader program
+
+  const vsSource = `
+    attribute vec4 aVertexPosition;
+
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+
+    void main() {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    }
+  `;
